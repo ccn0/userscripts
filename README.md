1ea2120f0f211a9aa31f21562edae1abf6ae1b6a
ccn0 <82831204+ccn0@users.noreply.github.com>
2024-04-28 00:47:11 -0500

    this is all for now

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# userscripts
-userscripts ive made of varying quality
+# Userscripts
+
+These are userscripts I made for use for myself, that I thought could be here.
+Also, if it opens 2 tabs, idk why it does that so don't blame it on me.
+
+## [Max Max](https://github.com/ccn0/userscripts/raw/main/scripts/maxmax.user.js)
+
+Removes the gradients, TV ratings, and auto skips intro.
+I'll add features as I need them.
+
+## [No Round Corners](https://github.com/ccn0/userscripts/raw/main/scripts/noroundcorners.user.js)
+
+No round corners, ever.
+
+## [YouTube Helper](https://github.com/ccn0/userscripts/raw/main/scripts/youtubehelper.user.js)
+
+Removes the Thanks, Voice search, and Join buttons.
+If you open settings it turns off the video previews.
+It tries to hide the shorts panel.
+The share link doesn't have the long token.
+All links don't have the redirect token.
---
+++ scripts/youtubehelper.user.js
@@ -0,0 +1,63 @@
+// ==UserScript==
+// @name         YouTube Helper
+// @namespace    ccn0
+// @version      2
+// @description  mods to make youtube better to use
+// @author       CCN0
+// @match        *://*.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?domain=youtube.com&sz=64
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function youTubeHelper() {
+        const voiceSearchButton = document.querySelector('button[aria-label="Search with your voice"]');
+        if (voiceSearchButton) {
+            voiceSearchButton.remove();
+        }
+
+        const joinButton = document.querySelector('button[aria-label="Join this channel"]');
+        if (joinButton) {
+            joinButton.remove();
+        }
+
+        const thanksButton = document.querySelector('button[aria-label="Thanks"]');
+        if (thanksButton) {
+            thanksButton.remove();
+        }
+
+        const links = document.querySelectorAll('a[href*="://www.youtube.com/redirect?"]');
+
+        links.forEach(link => {
+            const href = link.getAttribute('href');
+
+            if (href) {
+                const urlParams = new URLSearchParams(href);
+                const qParam = urlParams.get('q');
+
+                if (qParam) {
+                    const decodedQParam = decodeURIComponent(qParam);
+
+                    link.setAttribute('href', decodedQParam);
+                }
+            }
+        });
+
+        if (location.href.includes('account_playback')) {
+            const videoPreviewsSwitch = document.querySelector('*[aria-label="Video previews"]');
+            if (videoPreviewsSwitch.getAttribute('aria-pressed') == 'true') {
+                videoPreviewsSwitch.click();
+            }
+        }
+
+        document.getElementById('dismissible').style.display = 'none';
+
+        const shareUrlInput = document.getElementById('share-url');
+        let currentUrl = shareUrlInput.value;
+        currentUrl = currentUrl.replace(/(\?|\&)si=[^&]*/g, '');
+        shareUrlInput.value = currentUrl;
+    };
+    setInterval(youTubeHelper, 750);
+})();
