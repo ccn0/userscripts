b7f5d3fc66a50980d52659dcc22efcfb75562443
ccn0 <82831204+ccn0@users.noreply.github.com>
2024-05-03 21:00:12 -0500

    max max is now less lenient and just removes elements entirely

--- scripts/maxmax.user.js
+++ scripts/maxmax.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Max Max
 // @namespace    ccn0
-// @version      1
+// @version      2
 // @description  making max usable
 // @author       CCN0
-// @match        *://play.max.com/*
+// @match        *://*.max.com/*
 // @icon         https://www.max.com/favicon.ico
 // @grant        none
 // ==/UserScript==
@@ -19,21 +19,21 @@
         const advisories = document.querySelectorAll('[class^="RatingsAdvisoriesContainer-Beam-Web-Ent"]');
         if (advisories.length > 0) {
             advisories.forEach(element => {
-                element.style.display = "none";
+                element.remove();
             });
         } else {};
 
         var topgradient = document.querySelectorAll('[class^="TopGradient-Beam-Web-Ent"]');
         if (topgradient.length > 0) {
             topgradient.forEach(element => {
-                element.style.display = "none";
+                element.remove();
             });
         } else {};
 
         var bottomgradient = document.querySelectorAll('[class^="BottomGradient-Beam-Web-Ent"]');
         if (bottomgradient.length > 0) {
             bottomgradient.forEach(element => {
-                element.style.display = "none";
+                element.remove();
             });
         } else {};
     };
